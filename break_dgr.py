394783d6c8b66ca59ee8a1889dd5e28364c75b64
kriptaLev <lyukomarov@gmail.com>
2026-01-09 19:38:45 +0500

    Enhance break_dgr.py with size output
    Added print statements to show original and new file sizes after breaking the file.

--- break_dgr.py
+++ break_dgr.py
@@ -1,12 +1,14 @@
-# break_dgr.py
 with open("test3.dgr", "rb") as f:
     data = bytearray(f.read())
 
-# удаляем несколько байт в середине
+print("Original size:", len(data))
+
+# ломаем файл: удаляем 5 байт из середины
 mid = len(data) // 2
 del data[mid:mid+5]
 
 with open("test3_broken.dgr", "wb") as f:
     f.write(data)
 
-print("File broken: test3_broken.dgr")
+print("Broken file written: test3_broken.dgr")
+print("New size:", len(data))
